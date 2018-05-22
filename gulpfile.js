ceace23d9ff2670c32585bbf61eb1971d35b92a0
zky <250717403@qq.com>
2018-05-22 13:57:35 +0800

    first

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+//引入平台
+var gulp = require('gulp');
+
+//引入插件
+
+var minifyJs = require('gulp-uglify');
+var minifyCss = require('gulp-clean-css');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+var babel = require('gulp-babel');
+
+
+
+gulp.task('minifyCss',function(){
+    return gulp.src('./src/style/*.scss')
+        .pipe(sass())
+        .pipe(minifyCss())
+        .pipe(gulp.dest('./dist/css/'));
+})
+
+// gulp.task()
+
+gulp.task('minifyJs',function(){
+    return gulp.src('./src/js/*.js')
+        .pipe(babel())
+        .pipe(minifyJs())
+        .pipe(gulp.dest('./dist/js/'));
+})
+
+//自动刷新
+gulp.task('fresh',['minifyCss', 'minifyJs'],function(){
+    return gulp.src('./*.html').pipe(connect.reload());
+})
+
+
+gulp.task('default',['minifyCss'],function(){
+    gulp.watch(['./src/style/*.scss','./src/js/*.js','./*.html'],['fresh']);
+
+    connect.server({
+        port:8081,
+        livereload:true
+    })
+})
